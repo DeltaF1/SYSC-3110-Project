3d2393e3ef4dd6997bff8b5a005839a5d901a95a
Samuel Baumann <SamBaumann@cmail.carleton.ca>
2018-10-24 12:42:10 -0400

    created basic template for projectile plants

---
+++ src/Peashooter.java
@@ -0,0 +1,11 @@
+public class ProjectilePlant extends Plant{
+	
+	public ProjectilePlant(String name, int hp, int damage, int atkSpd, int cost) {
+		super(name, hp, damage, atkSpd, cost);
+	}
+	
+	public void update() {
+		//GameController.fireProjectile();
+	}
+	
+}
